75e206055e90cee2917297360c2555bfe5b8fca3
pustakalay <pustakalay2615@gmail.com>
2018-07-07 14:52:00 +0530

    admin url fix

--- website/website/urls.py
+++ website/website/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf.urls import include
 
 urlpatterns = [
-    url(r'', include('bookapp.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('bookapp.urls')),
+
 ]
