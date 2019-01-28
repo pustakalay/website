c580b3ffd1ce89798d871a858dfedfb9b784a48c
Tanay Paradkar <tanayparadkar@gmail.com>
2019-01-28 22:42:48 +0530

    Auto Download feature

--- website/utility/views.py
+++ website/utility/views.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
+from django.views.static import serve
 from forms import DocumentForm
 from django.core.files.storage import FileSystemStorage
 from joinImage import execute
 import os.path
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -30,6 +32,13 @@ def index(request):
                 fs.save(filename, file)
                 i = i + 1
             execute()
+#             file_name = 'Result.jpg'
+#             response = HttpResponse(mimetype='application/force-download')
+#             response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(file_name)
+#             response['X-Sendfile'] = smart_str(folder)
+#             return response
+            filepath = os.path.join(folder, 'Result.jpg')
+            return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
             return render(request, 'utility/index.html', {
         'form': form
     })
